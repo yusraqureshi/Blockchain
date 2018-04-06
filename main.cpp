40a3ac3e780cefe30e9a16daa3d53844b7321cb5
yusraqureshi <38133207+yusraqureshi@users.noreply.github.com>
2018-04-06 18:52:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,32 +8,23 @@
 using namespace std;
 
 int main()
-{
-    // Start Blockchain
-    Blockchain awesomeCoin;
-    
-    // Data for the first block
-    time_t data1Time;
-    TransactionData data1(1.5, "Rahul", "Yusra", time(&data1Time));
-    awesomeCoin.addBlock(data1);
-    
-    time_t data2Time;
-    TransactionData data2(0.2233, "Divya", "Arpit", time(&data2Time));
-    awesomeCoin.addBlock(data2);
-    
-    // Prints the awesomeCoin blockchain
-    awesomeCoin.printChain();
-    
-    printf("\nIs chain still valid? %d\n", awesomeCoin.isChainValid());
-    
-    // Someone's getting sneaky
-    Block *hackBlock = awesomeCoin.getLatestBlock();
-    hackBlock->data.amount = 10000; // 
-    hackBlock->data.receiverKey = "Jon";
- 
-    awesomeCoin.printChain();
-
-    printf("\nIs chain still valid? %d\n", awesomeCoin.isChainValid());
-    
-    return 0;
+{// Start Blockchain
+ Blockchain awesomeCoin;
+ // Data for the first block
+ time_t data1Time;
+ TransactionData data1(1.5, "Rahul", "Yusra", time(&data1Time));
+ awesomeCoin.addBlock(data1);
+ time_t data2Time;
+ TransactionData data2(0.2233, "Divya", "Arpit", time(&data2Time));
+ wesomeCoin.addBlock(data2);   
+ // Prints the awesomeCoin blockchain
+ awesomeCoin.printChain();
+ printf("\nIs this chain still valid? %d\n", awesomeCoin.isChainValid());
+ // Someone's getting sneaky
+ Block *hackBlock = awesomeCoin.getLatestBlock();
+ hackBlock->data.amount = 10000; // 
+ hackBlock->data.receiverKey = "Jon";
+ awesomeCoin.printChain();
+ printf("\nIs this chain still valid? %d\n", awesomeCoin.isChainValid());
+ return 0;
 }
